8640ce09d3b46dabbe22b8ccfaa4cb16b69bcdb7
Carlos Mendoza <carlosjmendoza91.dev@gmail.com>
2021-07-24 17:54:47 -0300

    Adiciona unit testing para classe DocumentTypeChecker

--- tests/Unit/DocumentTypeCheckerTest.php
+++ tests/Unit/DocumentTypeCheckerTest.php
@@ -1,1 +1,57 @@
 <?php
+
+namespace AppTests\Unit;
+
+use TestCase;
+use App\Http\Helpers\DocumentTypeChecker;
+
+class DocumentTypeCheckerTest extends TestCase
+{
+    /**
+     * Teste para remover caracteres especiais do cpf.
+     *
+     * @return void
+     */
+    public function testRemoveCaracteresCPF()
+    {
+        $documentTypeChecker = new DocumentTypeChecker();
+        $cpf = '063.913.247-27';
+        $this->assertEquals('06391324727', $documentTypeChecker->removeSpecialCharacters($cpf));
+    }
+
+    /**
+     * Teste para remover caracteres especiais do cnpj.
+     *
+     * @return void
+     */
+    public function testRemoveCaracteresCNPJ()
+    {
+        $documentTypeChecker = new DocumentTypeChecker();
+        $cnpj = '17.284.275/0001-05';
+        $this->assertEquals('17284275000105', $documentTypeChecker->removeSpecialCharacters($cnpj));
+    }
+
+    /**
+     * Teste para obter tipo de usuario a partir de cpf.
+     *
+     * @return void
+     */
+    public function testRetornaTipoDeUsuarioCPF()
+    {
+        $documentTypeChecker = new DocumentTypeChecker();
+        $cpf = '063.913.247-27';
+        $this->assertEquals(1, $documentTypeChecker->getDocumentType($cpf));
+    }
+
+    /**
+     * Teste para obter tipo de usuario a partir de cnpj.
+     *
+     * @return void
+     */
+    public function testRetornaTipoDeUsuarioCNPJ()
+    {
+        $documentTypeChecker = new DocumentTypeChecker();
+        $cnpj = '17.284.275/0001-05';
+        $this->assertEquals(0, $documentTypeChecker->getDocumentType($cnpj));
+    }
+}
