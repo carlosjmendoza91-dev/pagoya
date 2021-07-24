e81d29f8f4f7b51f382b031d45c7768035f41dcf
Carlos Mendoza <carlosjmendoza91.dev@gmail.com>
2021-07-24 19:43:21 -0300

    Atualizar nome Json Middleware

--- app/Http/Middleware/NormalizeRequestPayloadMiddleware.php
+++ app/Http/Middleware/NormalizeRequestPayloadMiddleware.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class JsonRequestMiddleware
+class NormalizeRequestPayloadMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
