98e2d356437c77020c5fb74ed57cabc382e8abf7
Carlos Mendoza <carlosjmendoza91.dev@gmail.com>
2021-07-25 15:30:51 -0300

    Adiciona mensagems de retorno

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,39 +10,31 @@ use App\Repositories\User\IUserRepository;
 
 class AuthController extends Controller
 {
+    const SIGNUP_SUCCESS = 'User was created successfully';
+    const VERIFY_CREDENTIALS = 'Please, verify your access credentials';
+
     protected $user = null;
 
     public function __construct(IUserRepository $IUserRepository)
     {
         $this->user = $IUserRepository;
-        $this->middleware('auth:api', ['except' => ['login','signup']]);
     }
 
     public function signup(SignUpRequest $signUpRequest)
     {
         $newUser = $this->user->create($signUpRequest->getParams());
-        return $this->returnResponse($newUser, 200);
+        return $this->returnResponse([], 200, self::SIGNUP_SUCCESS);
     }
 
     public function login(LoginRequest $loginRequest)
     {
         if (! $token = Auth::attempt($loginRequest->getCredentials()))
-            return $this->returnResponse([], 401, 'Unauthorized');
+            return $this->returnResponse([], 401, self::VERIFY_CREDENTIALS);
 
         return $this->returnResponse([
             'token' => $token,
             'token_type' => 'Bearer',
             'expires_in' => null
         ], 200);
-    }
-
-    /**
-     * Get user details.
-     *
-     * @param  Request  $request
-     */
-    public function me()
-    {
-        return response()->json(auth()->user());
     }
 }
