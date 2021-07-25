e9899d28c897db3843edaf6c63189aed37a69c0c
Carlos Mendoza <carlosjmendoza91.dev@gmail.com>
2021-07-25 15:39:35 -0300

    Adicionar verificacao de usuario logado

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,7 +6,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Requests\Transaction\TransactionRequest;
 use App\Repositories\Transaction\ITransactionRepository;
-use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
@@ -15,13 +14,21 @@ class TransactionController extends Controller
     public function __construct(ITransactionRepository $ITransactionRepository)
     {
         $this->transaction = $ITransactionRepository;
-        //$this->middleware('auth:api', ['except' => ['create']]);
-        //$this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     public function store(TransactionRequest $transactionRequest)
     {
-        $newTransaction = $this->transaction->create($transactionRequest->getParams());
-        return $this->returnResponse($newTransaction, 200);
+        $transactionRequestParams = $transactionRequest->getParams();
+        $this->checkLoggedInUser($transactionRequestParams['payer']);
+        $transactionResult = $this->transaction->create($transactionRequestParams);
+        return $this->returnResponse([], 200, $transactionResult->getMessage(), $transactionResult->getErrors());
     }
+
+    private function checkLoggedInUser(int $idPayer){
+        if(auth()->user()->getAuthIdentifier() !== $idPayer)
+            throw new \Exception('Voce nao possui privilegios para realizar essa operacao');
+        return true;
+    }
+
 }
