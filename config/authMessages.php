49bf47cd6887e27a4b4ebd8cc4893f7ed9b7395f
Carlos Mendoza <carlosjmendoza91.dev@gmail.com>
2021-07-25 19:12:14 -0300

    Ajustar chave de mensagem de verificar credenciais

--- config/authMessages.php
+++ config/authMessages.php
@@ -3,6 +3,6 @@ return [
     'signup_success' => 'User was created successfully',
     'login_success' => 'Login successful',
     'logout_success' => 'Logout successful',
-    'verify_credentias' => 'Please, verify your access credentials',
+    'verify_credentials' => 'Please, verify your access credentials',
     'user_no_priviledges' => 'User does not have priviledges to perform this operation'
 ];
