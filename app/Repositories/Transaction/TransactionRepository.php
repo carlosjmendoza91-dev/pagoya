aee50dcda5b62427da449340ca2147fc6ed47fe3
Carlos Mendoza <carlosjmendoza91.dev@gmail.com>
2021-07-25 01:51:23 -0300

    Adicionadas validacoes na hora de criar transacao
    - Valida se o usuario que envia o dinheiro e PJ
    - Validar se o usuario que envia tem dinheiro na sua carteira

--- app/Repositories/Transaction/TransactionRepository.php
+++ app/Repositories/Transaction/TransactionRepository.php
@@ -4,15 +4,43 @@
 namespace App\Repositories\Transaction;
 
 use App\Models\Transaction;
+use App\Repositories\User\IUserRepository;
+use App\Http\Helpers\DocumentTypeChecker;
 
 class TransactionRepository implements ITransactionRepository
 {
+    private $user = null;
 
+    public function __construct(IUserRepository $IUserRepository)
+    {
+        $this->user = $IUserRepository;
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function create(array $transactionData)
     {
+        $this->checkUserType($transactionData['payer']);
+        $this->checkUserBalance($transactionData['payer'], $transactionData['value']);
+
         $newTransference = new Transaction($transactionData);
         $newTransference->save();
         return $newTransference->toArray();
+    }
+
+    private function checkUserType(int $id)
+    {
+        $user = $this->user->getType($id);
+        if($user === DocumentTypeChecker::PJ_DOCUMENT_TYPE)
+            throw new \Exception('Usuario CPNJ nao pode enviar dinheiro');
+    }
+
+    private function checkUserBalance(int $id, float $amount)
+    {
+        $balance = $this->user->getBalance($id);
+        if(round($balance, 2) < round($amount, 2))
+            throw new \Exception('Usuario nao possui dinheiro para enviar');
     }
 
 }
