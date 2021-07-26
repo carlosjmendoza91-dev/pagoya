e43064b10a296162316e0f6fc31d9f113e5877c7
Carlos Mendoza <carlosjmendoza91.dev@gmail.com>
2021-07-25 22:06:56 -0300

    Remover metodos em desuso

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -28,19 +28,6 @@ class UserRepository implements IUserRepository
         return $newUser->toArray();
     }
 
-    public function verifyPassword(Array $userLogin)
-    {
-        $currentUser = $this->getUserByEmail($userLogin['email']);
-        if(Hash::check($userLogin['password'], $currentUser->password))
-            return $currentUser;
-        return null;
-    }
-
-    private function getUserByEmail(string $email){
-        return User::where('email', $email)->first();
-    }
-
-
     public function getType(int $id)
     {
         $user = User::where('id', $id)->first();
